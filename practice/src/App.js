38a371faf25800dc400dcca6f5c6c8edefd06054
kirnjiyun <kimjiyunee@naver.com>
2024-06-17 22:07:55 +0900

    곱하기 기능 추가

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
 export default function App() {
-    const [count, setCount] = useState(0);
-
+    const [count, setCount] = useState(1);
+    const multiply = () => {
+        setCount(count * 5);
+    };
     return (
         <div>
             숫자를 변하게 해봅시다!
             <div>{count}</div>
             <button>증가</button>
             <button>감소</button>
+            <button onClick={multiply}>곱하기 5</button>
         </div>
     );
 }
