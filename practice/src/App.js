85c377e36ebd774b357e1ff845004ff40161f169
unknown <sh911124@gmail.com>
2024-06-17 22:18:05 +0900

    감소 추가

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 
 export default function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            숫자를 변하게 해봅시다!
-            <div>{count}</div>
-            <button>증가</button>
-            <button>감소</button>
-        </div>
-    );
+  const decreaseCount = () => {
+    let num = count - 1;
+    setCount(num);
+  };
+
+  return (
+    <div>
+      숫자를 변하게 해봅시다!
+      <div>{count}</div>
+      <button>증가</button>
+      <button onClick={decreaseCount}>감소</button>
+    </div>
+  );
 }
