ff3f475686b5c94f4ae3e00a0def20616051b033
vedivero <vedivero@github.com>
2024-06-17 22:19:34 +0900

    additional number

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+import { additional } from "./additional.js"
 
 export default function App() {
     const [count, setCount] = useState(0);
+
+    const handleIncrease = () => {
+        setCount(count + 1);
+    };
 
     return (
         <div>
             숫자를 변하게 해봅시다!
             <div>{count}</div>
-            <button>증가</button>
+            <button onClick={handleIncrease}>증가</button>
             <button>감소</button>
         </div>
     );
